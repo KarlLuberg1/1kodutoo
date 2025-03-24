011430ceb0b43b52ae71d72031661b5de062dfa5
KL1 <karl@luberg.ee>
2025-03-24 19:04:45 +0200

    font.js comments added

--- font.js
+++ font.js
@@ -1,3 +1,5 @@
+//Suures osas tehtud lihtsalt copy paste värvide ja taustapiltide vahetamise funktsioonidest
+//ja siis tegin vastavad muudatused
 console.log("font.js loaded");
 
 const fontButton = document.getElementById('font-toggle');
@@ -14,6 +16,9 @@ let currentFontIndex = 0;
 
 fontButton.addEventListener('click', () => {
     currentFontIndex = (currentFontIndex + 1) % fonts.length;
+    
+    //need kaks rida olid chatgpt abiga tehtud, kuna ei saanud tööle alguses fontide vahetamist
+    //prompt: pastesin talle selle koodi ning veateate, mis konsool andis
     document.getElementById('container').style.setProperty('font-family', fonts[currentFontIndex], 'important');
     console.log("Font changed to:", fonts[currentFontIndex]);
 });
